6b117449a040d67c4bf53a6f92aa111d63f99b45
Uche Ozoemena <ucheozoemena@gmail.com>
2019-04-10 11:15:18 +0100

    Use CSS variables for settings page CSS

--- user-profile-setting.php
+++ user-profile-setting.php
@@ -46,14 +46,14 @@
                         <br>
                         Change passord:<br>
                         <input class="user-profile-input" type="text" name="lastname">
-                        
-                    </form> 
-                    
+
+                    </form>
+
                 </div>
                 <div class="d-flx sub">
-                    <span class="co-red">
+                    <button class="btn co-red">
                         Delete my account
-                    </span>
+                    </button>
                     <p class="lang">
                         Language
                         <span class="en">
@@ -68,7 +68,7 @@
                         Theme
                     </p>
                     <button class="drop user-profile-button">
-                        DragonBlade Slicer Supreme 2000  
+                        DragonBlade Slicer Supreme 2000
                         <svg width="14" height="12" viewBox="0 0 14 12" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M7 12L0.0717973 -6.02284e-07L13.9282 -1.81365e-06L7 12Z" fill="#9A9A9A"/>
                         </svg>
@@ -81,7 +81,7 @@
                     <div class="d-flx no-justify">
                         <div class="toggle">
                             <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="9" cy="9" r="9" fill="currentColor"/>
+                            <circle cx="9" cy="9" r="9" fill="black"/>
                             </svg>
                         </div>
                         <p class="ml-1">
